fe03b9711ad96314b8d4f514d658ad31635ecc32
kgazineu <kaiangazineu@gmail.com>
2024-06-26 04:17:48 -0300

    typeORM

--- store/src/app.module.ts
+++ store/src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostgresConfigService } from './configurations/postgres.config.service';
 import { ProductModule } from './products/products.module';
 import { UsersModule } from './users/user.module';
 
 @Module({
-  imports: [UsersModule, ProductModule],
+  imports: [
+    UsersModule,
+    ProductModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostgresConfigService,
+      inject: [PostgresConfigService],
+    }),
+  ],
 })
 export class AppModule {}
--- store/src/users/entities/user.entity.ts
+++ store/src/users/entities/user.entity.ts
@@ -1,6 +1,24 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({ name: 'users' })
 export class UserEntity {
+  @PrimaryGeneratedColumn('uuid', { name: 'id' })
   id: string;
-  email: string;
-  password: string;
+  @Column({ name: 'name', length: 100, nullable: false })
   name: string;
+  @Column({ name: 'name', length: 70, nullable: false })
+  email: string;
+  @Column({ name: 'name', length: 255, nullable: false })
+  password: string;
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: string;
+  @CreateDateColumn({ name: 'updated_at' })
+  updatedAt: string;
+  @CreateDateColumn({ name: 'deleted_at' })
+  deletedAt: string;
 }
